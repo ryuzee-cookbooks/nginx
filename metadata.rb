51efe5adc199f97b7d3973e59dcaf45bf58c227c
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-09-06 17:40:15 +0900

    fix yum version

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          'MIT'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
-depends          'yum', "~> 3.0.4"
+depends          'yum', "~> 3.3"
 depends          'yum-epel'
