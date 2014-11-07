f26c1f10ee6cf454b54babe9fc465cf35fe26857
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-11-07 18:24:14 +0900

    fix ca-certificates

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
-site :opscode
+source "http://api.berkshelf.com"
 
 metadata
+
+cookbook 'ca-certificates', git:"https://github.com/ryuzee-cookbooks/ca-certificates.git"
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'ryuzee@gmail.com'
 license          'MIT'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
+depends          'ca-certificates'
 depends          'yum', "~> 3.3"
 depends          'yum-epel'
